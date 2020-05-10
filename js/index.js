62e16e69fa8273e7d0e0f2d1947bad809c1b2721
Itallo Vidal <48143354+ividalll666@users.noreply.github.com>
2020-05-10 16:51:56 -0300

    Add files via upload
    Index ( Login ) e página inicial do player. Mais atualizacões em breve.

---
+++ js/index.js
@@ -0,0 +1,20 @@
+let menu = '';
+let i = 0;
+
+
+
+
+function esmaecer()
+{
+    for(i = 0; i < 8; i++)
+    {
+        menu = document.getElementsByClassName('nav')[i]; 
+        menu.style.animation = '1s ease 0s normal forwards opc';
+    }
+
+    setTimeout( function outraPag(){location.href = 'paginicial.html';},1100);
+
+}
+
+
+
